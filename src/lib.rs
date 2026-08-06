daf09c8438dfffabefbe5334db7cd049d25c39df
ポッチャマ <mercworld@pocha.moe>
2026-08-06 00:05:54 -0400

    oops i fucked up

--- src/lib.rs
+++ src/lib.rs
@@ -5,5 +5,5 @@ pub mod routes;
 pub mod websocket;
 
 pub mod mapdata {
-    include!(concat!(env!("OUT_DIR"), "\\cached_beat_saver_data.rs"));
+    include!(concat!(env!("OUT_DIR"), "/cached_beat_saver_data.rs"));
 }
